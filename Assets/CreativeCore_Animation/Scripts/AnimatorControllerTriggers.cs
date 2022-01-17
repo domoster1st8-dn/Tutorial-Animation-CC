cabb480bdec7a2dd30c29dba20e17f7c235d79e1
metqua007 <54363131+metqua007@users.noreply.github.com>
2022-01-17 23:42:22 +0700

    Finish Animation Custom
    Animation Dance, Animator controller

--- Assets/CreativeCore_Animation/Scripts/AnimatorControllerTriggers.cs
+++ Assets/CreativeCore_Animation/Scripts/AnimatorControllerTriggers.cs
@@ -30,11 +30,12 @@ public class AnimatorControllerTriggers : MonoBehaviour
         //To duplicate this code block, select the code between this comment
         if (Input.GetKeyDown(KeyCode.A))
         {
-            anim.SetTrigger("YOUR_TRIGGER_NAME_HERE_INSIDE_THE_QUOTES");
+            anim.SetTrigger("Dance");
         }
+        
         //And this comment, then hit CTRL/CMD C to copy, and CTRL/CMD D to paste
         //starting on the line below this one!
-        
+
 
     }
 }
